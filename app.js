8407fd4ebb3a0e8f3f0e96ef6cc583b4973503c2
Wawowee <123567512+Wawowee@users.noreply.github.com>
2025-10-15 23:23:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -86,8 +86,8 @@ export async function loadOpenCVOnce() {
 
 // Hidden canvas just for CV processing (not added to DOM)
 const work = document.createElement('canvas');
-work.width = 1280;  // you can try 640x480 later if corners are small
-work.height = 720;
+work.width = 480;  // you can try 640x480 later if corners are small
+work.height = 360;
 const wctx = work.getContext('2d', { willReadFrequently: true });
 
 //OpenCV Step 2 End
